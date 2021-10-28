2cf3d410997534e3660b5bc3c27c3cc0ddc20208
Thomas Mack <tmackcsb@gmail.com>
2021-10-28 18:54:19 +0100

    added some more stuff still very rough and idk what im doing

--- src/PebbleGame.java
+++ src/PebbleGame.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class PebbleGame {
+
+    ArrayList<ArrayList> players = new ArrayList<>();
+
+    //idk whats going on here
+    public void threads(int pNum){
+        for (int i = 0; i < pNum; i ++){
+            ArrayList<String> playerinfo = new ArrayList<>();
+            playerinfo.add("Player_" + String.valueOf(i));
+            players.add(playerinfo);
+        }
+    }
+
     public static void main(String[] args){
-        System.out.println("Testing github");
+        PebbleGame pg = new PebbleGame();
+        String players;
+        System.out.println("How many players?");
+        Scanner sc = new Scanner(System.in);
+        players = sc.nextLine();
+        int playInt = Integer.valueOf(players);
+
+        pg.threads(playInt);
+
+        Read read = new Read();
+        Write write = new Write(3);
     }
 }
