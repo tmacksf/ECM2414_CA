c757f552d536e9182fc2f7cd260b8b21dd4025f2
Pemacope <pedrocatarinopereira@hotmail.com>
2021-11-04 14:07:08 +0000

    Input done with error handling

--- src/PebbleGame.java
+++ src/PebbleGame.java
@@ -1,7 +1,8 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Collections;
 import java.util.Scanner;
 import java.util.ArrayList;
-import java.io.IOException;
 import java.util.concurrent.*;
 
 public class PebbleGame{
@@ -10,27 +11,56 @@ public class PebbleGame{
 
     public synchronized static void main(String[] args){
         PebbleGame pg = new PebbleGame();
+        int numPlayers = 0;
         String players;
-        System.out.println("How many players?");
-        Scanner sc = new Scanner(System.in);
-        players = sc.nextLine();
-        int playInt = Integer.valueOf(players);
+        //System.out.println is used for every new line to ensure the code is platform-independent
+        System.out.println("Welcome to the PebbleGame!!");
+        System.out.println("You will be asked to enter the number of players.");
+        System.out.println("And then for the location of three files in turn containing comma separated integer values for the pebble weights.");
+        System.out.println("The integer values must be strictly positive.");
+        System.out.println("The game will then be simulated, and output written to files in this directory");
+        System.out.println();
 
-        //pg.threads(playInt);
+        //Input for number of players
+        while (numPlayers < 1){
+            System.out.println("Please enter the number of players:");
+            Scanner sc = new Scanner(System.in);
+            players = sc.nextLine();
+            try {
+                numPlayers = Integer.parseInt(players);
+            }catch(Exception e){
+                if (players.equals("E")) System.exit(0);
+            }
+        }
 
-        Read read = new Read(playInt);
-        Write write = new Write(playInt);
+        //Input for files
+        ArrayList<ArrayList> fileInfo = new ArrayList<>();
 
-        ArrayList<Integer> bagA = read.getFileInfo().get(0);
-        ArrayList<Integer> bagB = read.getFileInfo().get(1);
-        ArrayList<Integer> bagC = read.getFileInfo().get(2);
+        for (int i = 0; i < 3; i++){
+            ArrayList<Integer> temp = new ArrayList<>();
+            boolean fileFlag = false;
+            while(!fileFlag){
+                System.out.println("Please enter location of bag number " + i + " to load");
+                Scanner sc = new Scanner(System.in);
+                String fileName = sc.nextLine();
+                try {
+                    Scanner f = new Scanner(new File(fileName));
+                    while (f.hasNextLine()) {
+                        String weightValues = f.nextLine();
+                        String[] arrOfWeighValues = weightValues.split(",", 0);
+                        for (String str : arrOfWeighValues) temp.add(Integer.valueOf(str));
+                        fileFlag = true;
+                        fileInfo.add(temp);
+                    }
+                }catch(Exception e){
+                    if (fileName.equals("E")) System.exit(0);
+                }
+            }
+        }
 
-        ArrayList<Integer> bagX = new ArrayList<>();
-        ArrayList<Integer> bagY = new ArrayList<>();
-        ArrayList<Integer> bagZ = new ArrayList<>();
-
-        Player p = new Player();
-        p.run();
+        ArrayList<Integer> bagA = fileInfo.get(0);
+        ArrayList<Integer> bagB = fileInfo.get(1);
+        ArrayList<Integer> bagC = fileInfo.get(2);
 
     }
 }
